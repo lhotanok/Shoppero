94e36f6ad91f9bbdcad80aa19d82680cbcd0c7e8
Kristýna Lhoťanová <kristyna.posta@seznam.cz>
2021-07-30 13:22:40 +0200

    Change web app title into Shoppero

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<title>Final Assignment - Shopping List</title>
+	<title>Shoppero</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="style/style.css">
