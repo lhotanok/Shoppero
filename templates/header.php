b4d1f6913b31a7cf6b15b1589e9e72aa6c99d179
Kristýna Lhoťanová <kristyna.posta@seznam.cz>
2021-07-30 13:53:12 +0200

    Add application icon

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<title>Shoppero</title>
+	<link rel="icon" href="style/icon.png">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="style/style.css">
